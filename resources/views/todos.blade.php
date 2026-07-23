f1f56a50e3c31cc8db52349c8a5780971397151f
Ben Croker <ben@putyourlightson.com>
2026-07-23 21:50:23 +0200

    Clean up HTML

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -1,18 +1,12 @@
 <!doctype html>
-<html lang="en"
-      data-match-media:dark="prefers-color-scheme: dark"
-      data-attr:data-theme="$dark ? 'dark' : 'light'"
->
-
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Stardust Todo List Demo</title>
+    <title>Stardust Todos</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-
 <body data-init="{{ datastar()->action(['TodosController', 'updates'], [], ['retry' => 'always']) }}">
     @include('components.todo-items', ['todoItems' => $todoItems])
 </body>
-
 </html>
